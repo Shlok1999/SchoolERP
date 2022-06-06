8310aeebee81cf8c0bc9f81df3330ef0006c70d6
shlok1999 <09shlok1999ae1@gmail.com>
2022-06-06 19:46:34 +0530

    done

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 // This is the main backend file of the School Management Project
 // Mostly CRUD based work by using mongo db as the database
 
-const expresss = require('express')
-const app = expresss()
+const express = require('express')
+const app = express()
+const cors = require('cors')
 
-let port = process.env.PORT || 4040
+require('dotenv').config()
+
+const port = process.env.PORT || 4000
+
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res)=>{
-    res.send('Hello')
+    res.send('Hello') 
 })
+
 
 
 app.listen(port, ()=>{
     console.log(`http://localhost:${port}`)
 })
-
---
+++ models/admin.models.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const AdminSchema = new Schema({
+    email: {
+        type: String,
+        required:true,
+    },
+    username:{
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    confirmPass: {
+        type: String,
+        required: true
+    }
+})
+
+const Admin = mongoose.model('Admin', AdminSchema)
+module.exports = Admin
